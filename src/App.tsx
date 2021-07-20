18b4480cfecefd5544d01416bb33e4711f41953a
Oliver <alonzofe@adobe.com>
2021-07-20 11:45:00 -0400

    Add GIHPY search following unsplash tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,42 @@
 // == External
-import React, { FC } from 'react'
+import React, { FC, Component } from 'react'
+import { Container, Row, Col, Tabs, Tab, InputGroup, FormControl, Button} from 'react-bootstrap'
 
 // == App
 import { Counter } from './Counter'
+import { SearchBox } from './SearchBox'
+
 
 export const App:FC = () => {
 
-  return <div>
-    <h1>hello world</h1>
-    <Counter />
-  </div>
+  return <Container fluid>
+    {/*<Row>
+      <Col>
+        <h1>hello world</h1>
+        <Counter />
+      </Col>
+    </Row>*/}
+    <Row>
+      <Col>
+        <Row id="video-player">
+        Video Player
+        </Row>
+        <Row id="search-box">
+          <Container>
+            <Tabs defaultActiveKey="gifs" id="uncontrolled-tab-example" className="mb-3">
+              <Tab eventKey="gifs" title="GIFs">
+                <SearchBox/>
+              </Tab>
+              <Tab eventKey="emojis" title="Emojis">
+
+              </Tab>
+            </Tabs>
+          </Container>
+        </Row>
+      </Col>
+      <Col id="sound-events">
+      Sound Events
+      </Col>
+    </Row>
+  </Container>
 }
