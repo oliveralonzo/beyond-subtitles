eacbb18a6d6e68405decb0f7c0937922bcc969a1
Oliver <alonzofe@adobe.com>
2021-08-16 16:41:42 -0400

    Update dummy data

--- src/Dummy.tsx
+++ src/Dummy.tsx
@@ -3,21 +3,21 @@ export const sounds = {
     {
       "startTime": 0,
       "endTime": 6000,
-      "label": "Trumpet playing",
+      "label": "Trumpet",
       "automatic": true
     },
     {
       "startTime": 13000,
       "endTime": 15000,
-      "label": "Train honking",
+      "label": "Train",
       "automatic": true
     }
   ],
-  "Dog": [
+  "TikTok (Dog)": [
     {
       "startTime": 7000,
       "endTime": 9000,
-      "label": "Dog barking",
+      "label": "Dog",
       "automatic": true
     },
     {
@@ -35,30 +35,7 @@ export const sounds = {
     {
       "startTime": 12000,
       "endTime": 14000,
-      "label": "Glass shattering",
-      "automatic": true
-    }
-  ],
-  "yapeng": [
-    {
-      "id": 0,
-      "startTime": 2000,
-      "endTime": 5000,
-      "tags": "food bowl, girl, computer, texts",
-      "automatic": true
-    },
-    {
-      "id": 1,
-      "startTime": 6000,
-      "endTime": 8000,
-      "tags": "girl, computer, scene change",
-      "automatic": true
-    },
-    {
-      "id": 2,
-      "startTime": 9000,
-      "endTime": 23000,
-      "tags": "girl, computer, texts",
+      "label": "Glass",
       "automatic": true
     }
   ],
@@ -66,13 +43,13 @@ export const sounds = {
     {
       "startTime": 0,
       "endTime": 2000,
-      "label": "Baby crying",
+      "label": "Baby",
       "automatic": true,
     },
     {
       "startTime": 4000,
       "endTime": 7000,
-      "label": "Baby crying",
+      "label": "Baby",
       "automatic": true,
     },
     {
@@ -136,136 +113,138 @@ export const sounds = {
     {
       "startTime": 1000,
       "endTime": 1300,
-      "label": "Speech"
+      "label": "Speech",
+      "automatic": true
     },
     {
       "startTime": 4300,
       "endTime": 4600,
-      "label": "Speech"
+      "label": "Speech",
+      "automatic": true
     },
     {
       "startTime": 5500,
       "endTime": 6200,
-      "label": "Speech"
+      "label": "Speech",
+      "automatic": true
     },
     {
       "startTime": 6500,
       "endTime": 7200,
-      "label": "Screaming"
+      "label": "Screaming",
+      "automatic": true
     },
     {
       "startTime": 10500,
       "endTime": 11200,
-      "label": "Screaming"
+      "label": "Screaming",
+      "automatic": true
     },
     {
       "startTime": 5800,
       "endTime": 6100,
-      "label": "Bleat"
+      "label": "Bleat",
+      "automatic": true
     },
     {
       "startTime": 5800,
       "endTime": 6100,
-      "label": "Sheep"
+      "label": "Sheep",
+      "automatic": true
     },
     {
       "startTime": 7300,
       "endTime": 7600,
-      "label": "Fowl"
+      "label": "Fowl",
+      "automatic": true
     },
     {
       "startTime": 2800,
       "endTime": 3300,
-      "label": "Owl"
+      "label": "Owl",
+      "automatic": true
     }
   ],
   "BBC Automatic": [
     {
-      "startTime": 6800,
-      "endTime": 13700,
-      "label": "Speech"
-    },
-    {
-      "startTime": 15300,
-      "endTime": 23900,
-      "label": "Speech"
-    },
-    {
       "startTime": 2500,
       "endTime": 3000,
-      "label": "Meow"
+      "label": "Meow",
+      "automatic": true
     },
     {
       "startTime": 100,
       "endTime": 500,
-      "label": "Insect"
+      "label": "Insect",
+      "automatic": true
     },
     {
       "startTime": 100,
       "endTime": 400,
-      "label": "Mosquito"
+      "label": "Mosquito",
+      "automatic": true
     },
     {
       "startTime": 100,
       "endTime": 400,
-      "label": "Fly, housefly"
+      "label": "Fly, housefly",
+      "automatic": true
     },
     {
       "startTime": 400,
       "endTime": 3900,
-      "label": "Music"
+      "label": "Music",
+      "automatic": true
     },
     {
       "startTime": 4400,
       "endTime": 5400,
-      "label": "Music"
+      "label": "Music",
+      "automatic": true
     },
     {
       "startTime": 400,
       "endTime": 800,
-      "label": "Trumpet"
+      "label": "Trumpet",
+      "automatic": true
     },
     {
       "startTime": 2100,
       "endTime": 2400,
-      "label": "Trumpet"
+      "label": "Trumpet",
+      "automatic": true
     },
     {
       "startTime": 13900,
       "endTime": 14200,
-      "label": "Train"
+      "label": "Train",
+      "automatic": true
     },
     {
       "startTime": 5900,
       "endTime": 6200,
-      "label": "Beep, bleep"
+      "label": "Beep, bleep",
+      "automatic": true
     }
   ],
   "YouTube Automatic": [
     {
-      "startTime": 12500,
-      "endTime": 14400,
-      "label": "Speech"
-    },
-    {
-      "startTime": 20500,
-      "endTime": 27200,
-      "label": "Speech"
+      "startTime": 4500,
+      "endTime": 4900,
+      "label": "Horse",
+      "automatic": true
     },
     {
       "startTime": 4500,
       "endTime": 4900,
-      "label": "Horse"
-    },
-    {
-      "startTime": 4500,
-      "endTime": 4900,
-      "label": "Neigh, whinny"
+      "label": "Neigh, whinny",
+      "automatic": true
     },
     {
       "startTime": 5700,
       "endTime": 6200,
-      "label": "Music"
+      "label": "Music",
+      "automatic": true
     }
   ]
 }
